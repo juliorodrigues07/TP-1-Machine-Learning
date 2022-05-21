60234457af241ce872557f5b05c88f3c19ed9a63
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2022-05-20 23:30:15 -0300

    feat: Adicionados TODO's para auxiliar no desenvolvimento da 3ª etapa do TP

--- tp1_ML.py
+++ tp1_ML.py
@@ -20,9 +20,10 @@ def pre_processing(attributes, classes):
                                              remainder='passthrough')
     attributes = l_encoder_attributes.fit_transform(attributes).toarray()
 
+    # TODO: Ajustar estratégia de preenchimento de valores ausentes ('mean', 'median', 'most_frequent', 'constant')
     # Preenche missing values com a média
-    fill_vm = SimpleImputer(strategy='mean')
-    attributes = fill_vm.fit_transform(attributes)
+    fill_mv = SimpleImputer(strategy='mean')
+    attributes = fill_mv.fit_transform(attributes)
 
     # Discretiza os valores da classe
     l_encoder_classes = LabelEncoder()
@@ -48,6 +49,7 @@ def decision_tree(classifier, training_attributes, training_classes, test_attrib
 
 def ordinary_tests(attributes, classes):
 
+    # TODO: Ajustar a proporção dos conjuntos de treinamento e teste para aplicar o modelo de aprendizado
     # Divisão do conjunto de treinamento e teste (80% e 20%)
     training_attributes, test_attributes, training_classes, test_classes = train_test_split(attributes, classes,
                                                                                         test_size=0.2, random_state=0)
@@ -62,9 +64,9 @@ def cv_tests(attributes, classes):
 
     classifier = tree.DecisionTreeClassifier(criterion='entropy')
 
+    # TODO: Ajustar escolha do valor de k para aplicação de testes com validação cruzada
     scores = cross_val_score(estimator=classifier, X=attributes, y=classes, cv=10, n_jobs=-1)
 
-    # print(scores)
     result = np.mean(scores)
     print('\nValidação Cruzada: ' + str(result) + '\n')
 
@@ -77,7 +79,7 @@ def plot_per_column_distribution(df, nGraphShown, nGraphPerRow):
     columnNames = list(df)
     nGraphRow = (nCol + nGraphPerRow - 1) / nGraphPerRow
 
-    plt.figure(num = None, figsize = (6 * nGraphPerRow, 8 * nGraphRow), dpi = 80, facecolor = 'w', edgecolor = 'k')
+    plt.figure(num=None, figsize=(6 * nGraphPerRow, 8 * nGraphRow), dpi=80, facecolor='w', edgecolor='k')
     for i in range(min(nCol, nGraphShown)):
 
         plt.subplot(int(nGraphRow), int(nGraphPerRow), i + 1)
@@ -90,9 +92,10 @@ def plot_per_column_distribution(df, nGraphShown, nGraphPerRow):
             columnDf.hist()
 
         plt.ylabel('counts')
-        plt.xticks(rotation = 90)
+        plt.xticks(rotation=90)
         plt.title(f'{columnNames[i]} (column {i})')
-    plt.tight_layout(pad = 1.0, w_pad = 1.0, h_pad = 1.0)
+
+    plt.tight_layout(pad=1.0, w_pad=1.0, h_pad=1.0)
     plt.show()
 
 
@@ -107,7 +110,7 @@ def plot_correlation_matrix(df, graphWidth):
 
     corr = df.corr()
     plt.figure(num=None, figsize=(graphWidth, graphWidth), dpi=80, facecolor='w', edgecolor='k')
-    corrMat = plt.matshow(corr, fignum = 1)
+    corrMat = plt.matshow(corr, fignum=1)
 
     plt.xticks(range(len(corr.columns)), corr.columns, rotation=90)
     plt.yticks(range(len(corr.columns)), corr.columns)
@@ -119,23 +122,28 @@ def plot_correlation_matrix(df, graphWidth):
 
 def main():
 
+    # TODO: Avaliar desempenho do método de ML e possível aplicação de outros
     pokemon_dataset = pd.read_csv('pokemon.csv')
     pokemon_dataset.dataframeName = 'pokemon.csv'
 
+    # Divisão da base de dados entre atributos e classe
     model_predictors = pokemon_dataset.drop(['type1', 'type2'], axis='columns')
     model_class = pokemon_dataset['type1']
 
+    # Pré-Processamento
     model_predictors, model_class = pre_processing(model_predictors.copy(), model_class.copy())
 
+    # Treinamento normal
     ordinary_tests(model_predictors, model_class)
 
+    # Treinamento com validação cruzada
     cv_tests(model_predictors, model_class)
 
-    #fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(8, 8), dpi=300)
-    #tree.plot_tree(classifier, filled=True)
-    #fig.savefig('tree.png')
+    # fig, _ = plt.subplots(nrows=1, ncols=1, figsize=(8, 8), dpi=300)
+    # tree.plot_tree(classifier, filled=True)
+    # fig.savefig('tree.png')
 
-    #plot_per_column_distribution(pokemon_dataset, 24, 4)
+    # plot_per_column_distribution(pokemon_dataset, 24, 4)
     plot_correlation_matrix(pokemon_dataset, 8)
 
 
