6f95b6a14e3026900e9609d0358d49a3bf82228e
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2022-06-05 00:29:05 -0300

    feat: Agora executa testes com validação cruzada

--- pokemon_random-forest.py
+++ pokemon_random-forest.py
@@ -3,6 +3,7 @@ from ml_methods import plot_learning_curve
 from ml_methods import dataset_divisor
 from ml_methods import pre_processing
 from ml_methods import random_forest
+from ml_methods import cv_tests
 
 
 def main():
@@ -16,12 +17,15 @@ def main():
     # Pré-Processamento
     model_predictors, model_class = pre_processing(model_predictors.copy(), model_class.copy())
 
-    # Treinamento do modelo
+    # Treinamento normal
     random_forest(model_predictors, model_class, class_names)
 
+    # Treinamento com validação cruzada
+    algorithm = RandomForestClassifier(criterion='entropy')
+    cv_tests(model_predictors, model_class, algorithm)
+
     # Plot das curvas de aprendizado (Análise de overfitting e underfitting)
-    # algorithm = RandomForestClassifier()
-    # plot_learning_curve(model_predictors, model_class, algorithm, 18)
+    # plot_learning_curve(model_predictors, model_class, algorithm, 40)
 
 
 if __name__ == '__main__':
